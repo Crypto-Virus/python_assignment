fcd8abda586ce392c41cbb95219e5eb7bc31c346
Virus <zxcv.fghj.uiop@gmail.com>
2023-02-24 21:35:51 +0900

    Cleanup code

--- financial/crud.py
+++ financial/crud.py
@@ -6,53 +6,12 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 
-def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
-
-
-def get_user_by_email(db: Session, email: str):
-    return db.query(models.User).filter(models.User.email == email).first()
-
-
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.User).offset(skip).limit(limit).all()
-
-
-def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
-
-
-def get_items(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Item).offset(skip).limit(limit).all()
-
-
-def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
-    db_item = models.Item(**item.dict(), owner_id=user_id)
-    db.add(db_item)
-    db.commit()
-    db.refresh(db_item)
-    return db_item
-
-
-def get_data(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Entry).offset(skip).limit(limit).all()
-
-
-def insert_data(db: Session, entries: List[schemas.Entry]):
+def insert_financial_data(db: Session, entries: List[schemas.Entry]):
     db_entries = [models.Entry(**entry.dict()) for entry in entries]
-    db.add_all(db_entries)
+    for entry in db_entries:
+        db.merge(entry)
     db.commit()
     return db_entries
-
-
-def delete_data(db: Session):
-    db.query(models.Entry).delete()
-    db.commit()
 
 
 def get_financial_data(
@@ -72,6 +31,7 @@ def get_financial_data(
             query = query.filter(models.Entry.date <= end_date)
 
     return query.offset(skip).limit(limit).all()
+
 
 def get_financial_data_count(db: Session):
     return db.query(models.Entry).count()
--- financial/schemas.py
+++ financial/schemas.py
@@ -3,41 +3,6 @@ from datetime import date
 
 from pydantic import BaseModel
 
-
-class ItemBase(BaseModel):
-    title: str
-    description: Union[str, None] = None
-
-
-class ItemCreate(ItemBase):
-    pass
-
-
-class Item(ItemBase):
-    id: int
-    owner_id: int
-
-    class Config:
-        orm_mode = True
-
-
-class UserBase(BaseModel):
-    email: str
-
-
-class UserCreate(UserBase):
-    password: str
-
-
-class User(UserBase):
-    id: int
-    is_active: bool
-    items: List[Item] = []
-
-    class Config:
-        orm_mode = True
-
-
 class Entry(BaseModel):
     symbol: str
     date: date
