faf9eeb0603cbbe97efb186862c309ccadac6957
Virus <zxcv.fghj.uiop@gmail.com>
2023-02-24 22:09:39 +0900

    Support api endpoints with or without trailing slash

--- financial/main.py
+++ financial/main.py
@@ -23,6 +23,7 @@ def get_db():
     finally:
         db.close()
 
+@app.get('/api/financial_data')
 @app.get('/api/financial_data/')
 def get_financial_data(
     limit: int = 5,
@@ -50,6 +51,7 @@ def get_financial_data(
         }
     }
 
+@app.get('/api/statistics')
 @app.get('/api/statistics/')
 def get_statistics(
     symbol: str,
