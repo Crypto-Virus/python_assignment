f335794df6a910fb526c06121d474734afa9c5a0
Virus <zxcv.fghj.uiop@gmail.com>
2023-02-27 12:18:16 +0900

    Create and run first migration

---
+++ alembic/versions/b0f08f76df56_create_tables.py
@@ -0,0 +1,24 @@
+"""create tables
+
+Revision ID: b0f08f76df56
+Revises: 
+Create Date: 2023-02-27 12:16:46.505191
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b0f08f76df56'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
